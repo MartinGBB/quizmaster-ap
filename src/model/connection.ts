7e17af477c3ffae1c8a21f7c09ee297070e912a3
Martin <escorpmartin97@gmail.com>
2024-01-06 01:48:26 -0300

    refator: refatoring connection

--- src/model/connection.ts
+++ src/model/connection.ts
@@ -1,17 +1,18 @@
 import mysql from 'mysql'
 
-let connection = null
+export const connectionDB = async () => {
+  try {
+    const connection = mysql.createConnection({
+      host: 'localhost',
+      port: 3001,
+      user: 'martinb',
+      database: 'quizmaster',
+    })
+    await connection.connect()
+    console.log('Conectado a la base de datos')
 
-try {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3001,
-    user: 'martinb',
-    database: 'quizmaster',
-  })
-
-  connection.connect()
-  console.log('Conectado a la base de datos')
-} catch (error) {
-  console.error('Error al conectar a la base de datos:', error)
+    return connection
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error)
+  }
 }
