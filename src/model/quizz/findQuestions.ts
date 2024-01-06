e33a3c74c5f43c6dffed6e404c4bac3a38a1a8d9
Martin <escorpmartin97@gmail.com>
2024-01-06 01:52:26 -0300

    fix: change find users to find questions database

--- src/model/quizz/findQuestions.ts
+++ src/model/quizz/findQuestions.ts
@@ -3,11 +3,11 @@ import { connectionDB } from '../connection'
 export const allItems = async () => {
   try {
     const db = await connectionDB()
-    const query = await db?.query('SELECT * FROM quizmaster.users')
+    const query = await db?.query('SELECT * FROM quizmaster.questions')
 
     return [query]
   } catch (error) {
-    console.error('Error fetching users:', error)
+    console.error('Error fetching questions:', error)
     throw error
   }
 }
