3ff784a45680b4b49bbdb56344cbc63fec53468a
Martin <escorpmartin97@gmail.com>
2024-01-17 18:23:33 -0300

    refator: create entities folder to organize entities

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { root } from '../controller/root'
+import { root } from '../entities/root'
 import { handleErrorApi } from '../middlewares/handleErrorApi'
 
 export const app = express()
--- src/entities/root.ts
+++ src/entities/root.ts
@@ -1,7 +1,7 @@
 import express from 'express'
-import { login } from '../Users/controller/login'
-import { router as questions } from '../QuestionsQuiz/controller/router'
-import { router as quiz } from '../Quiz/controller/router'
+import { login } from './Users/controller/login'
+import { router as questions } from './QuestionsQuiz/controller/router'
+import { router as quiz } from './Quiz/controller/router'
 
 export const root = express.Router({ mergeParams: true })
 
