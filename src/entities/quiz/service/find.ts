ef89f7ac2c275b0a66b7bc21dc97b2cfcb6cd03d
Martin <escorpmartin97@gmail.com>
2024-01-19 15:03:50 -0300

    feat: implment finid service

--- src/entities/quiz/service/find.ts
+++ src/entities/quiz/service/find.ts
@@ -1,7 +1,43 @@
 import { find as findModel } from '../model/find'
 import { QuizDataResult } from '../../../types/quiz.interface'
+import { ZodError, z } from 'zod'
 
-export function find(questionId: number): Promise<QuizDataResult[] | null> {
+const QuizIdSchema = z.object({
+  correct_answer: z.number({
+    invalid_type_error: 'Id deve ser um número',
+    required_error: 'Id é requerido',
+  }),
+})
+
+function validateId(id: number) {
+  try {
+    const validationId = QuizIdSchema.parse(id)
+    return validationId
+  } catch (error) {
+    if (error instanceof ZodError) {
+      const { code, message } = error.errors[0]
+      return { code, message }
+    } else {
+      console.error('Error de validación:', error)
+      throw new Error('Error de validación')
+    }
+  }
+}
+
+export function isValidQuizDataResult(data: QuizDataResult[] | null): boolean {
+  return !!data
+}
+
+export async function find(questionId: number, confirmId = false) {
+  const validateInput = validateId(questionId)
+
+  if ('code' in validateInput) return validateInput
+
   const result: Promise<QuizDataResult[] | null> = findModel(questionId)
+
+  if (confirmId) {
+    const quizExist = isValidQuizDataResult(await result)
+    return quizExist
+  }
   return result
 }
